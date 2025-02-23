b1e912411970444c0b3d202dabbd838eb6031b23
Juana Saavedra <juanavsaavedrar@gmail.com>
2025-02-23 16:10:48 -0500

    Cambio a POO
    Cambio de los menús a programación orientada. En la clase padre, se almacenan los métodos que tienen todos los hijos en común.

--- main.py
+++ main.py
@@ -1,165 +1,220 @@
-from time import sleep
+from time import sleep #Función para retardar la ejecución de un programa
+#Programación modular - Importación de modulos necesarios
 from baseDatos import conectar_bd, crear_tablas
 from productos import crear_nuevo_producto, actualizar_producto, consultar_producto
 from clientes import crear_nuevo_cliente, actualizar_direccion_cliente, consultar_cliente
 from compras import finalizar_compra, quitar_producto, añadir_producto, mostrar_productos
 
-def imprimir_bienvenida():
-    print("\033[1;33m")
-    print("🍺════════════════════════════════════════════════════🍺")
-    print("      ✨ Bienvenido a la Cervecería Artesanal ✨       ")
-    print("🍺════════════════════════════════════════════════════🍺\033[0m")
+class Menu:
+    def __init__(self, con=None, titulo="", color="\033[0m"):
+        # Conexión a la base de datos y atributos visuales comunes
+        self.con = con
+        self.titulo = titulo  # Título del menú
+        self.color = color    # Color para presentación en consola
+        self.opciones = []    # Lista de opciones
 
-def menu_principal():
-    con = conectar_bd()  
-    if con:
-        crear_tablas(con)
+    def mostrar_encabezado(self):
+        # Muestra un encabezado basado en el título y el color definidos
+        print(self.color)
+        if self.titulo:
+            print(f"===== {self.titulo} =====")
+        print("\033[0m", end="")  # Reinicia el color de la consola
+
+    def solicitar_opcion(self):
+        # Solicita una opción al usuario
+        return input("\nSeleccione una opción: ")
+
+    def run(self):
+        # Método para elevar cuando no se ha implementado un método o función
+        raise NotImplementedError("El método run debe ser implementado en la subclase.")
+
+class MenuPrincipal(Menu):
+    def __init__(self):
+        # Establece la conexión y prepara la base de datos
+        con = conectar_bd()
+        if con:
+            crear_tablas(con)
+        super().__init__(con, titulo="Bienvenido a la Cervecería Artesanal", color="\033[1;33m")
+
+    def imprimir_bienvenida(self):
+        print(self.color)
+        print("🍺════════════════════════════════════════════════════🍺")
+        print("      ✨ Bienvenido a la Cervecería Artesanal ✨       ")
+        print("🍺════════════════════════════════════════════════════🍺\033[0m")
+
+    def run(self):
+        # Bucle principal para el acceso a los diferentes menús
         while True:
-            imprimir_bienvenida()
+            self.imprimir_bienvenida()
             print("\n📌 Opciones disponibles:")
             print("1️⃣   Iniciar sesión como administrador")
             print("2️⃣   Iniciar sesión como cliente")
             print("3️⃣   Salir del sistema")
-
-            seleccion = input("\n Seleccione una opción: ")
-
+            seleccion = input("\nSeleccione una opción: ")
+            #Validación de credenciales de administrador
             if seleccion == "1":
                 pwd = input("\n🔑 Ingrese la contraseña de administrador: ")
-                if pwd == "admin123": 
-                    while True:
-                        print("\033[1;34m\n ════════════════════════════")
-                        print("      🛠 MENÚ ADMINISTRADOR      ")
-                        print("══════════════════════════════ \033[0m")
-                        print("1️⃣   Menú de Productos")
-                        print("2️⃣   Menú de Clientes")
-                        print("3️⃣  🔙 Volver")
-
-                        opcion = input("\n Seleccione una opción: ")
-
-                        if opcion == "1":
-                            menu_productos(con)
-                        elif opcion == "2":
-                            menu_clientes(con)
-                        elif opcion == "3":
-                            print("👋 Volviendo al menú principal...")
-                            sleep(1)
-                            break
-                        else:
-                            print("⚠️ Opción no válida, intente nuevamente.")
-
+                if pwd == "admin123":
+                    MenuAdmin(self.con).run()
                 else:
                     print("❌ Contraseña incorrecta")
-
             elif seleccion == "2":
-                while True:
-                    print("\033[1;32m\n ═════════════════════════════")
-                    print("        👤 MENÚ CLIENTE        ")
-                    print("════════════════════════════ \033[0m")
-                    print("1️⃣   Registrarse")
-                    print("2️⃣   Actualizar dirección")
-                    print("3️⃣   Hacer una compra")
-                    print("4️⃣  🔙 Volver")
-
-                    opcion = input("\n Seleccione una opción: ")
-
-                    if opcion == "1":
-                        crear_nuevo_cliente(con)
-                    elif opcion == "2":
-                        actualizar_direccion_cliente(con)
-                    elif opcion == "3":
-                        menu_compras(con)
-                    elif opcion == "4":
-                        print("👋 Volviendo al menú principal...")
-                        sleep(1)
-                        break
-                    else:
-                        print("⚠️ Opción no válida, intente nuevamente.")
-
+                MenuCliente(self.con).run()
             elif seleccion == "3":
                 print("👋 Gracias por visitarnos. ¡Salud! 🍻")
-                con.close()
+                self.con.close()
                 break
             else:
                 print("⚠️ Seleccione una opción válida.")
 
-# ================= MENÚ DE PRODUCTOS ==================
-def menu_productos(con):
-    while True:
-        print("\033[1;35m\n ══════════════════════════════════")
-        print("       📦 MENÚ DE PRODUCTOS       ")
-        print("═════════════════════════════════ \033[0m")
-        print("1️⃣   Agregar producto")
-        print("2️⃣   Actualizar producto")
-        print("3️⃣  🔍 Consultar producto")
-        print("4️⃣  🔙 Volver al Menú Principal")
+class MenuAdmin(Menu):
+    def __init__(self, con):
+        # Configura el menú de administrador con sus atributos heredados
+        super().__init__(con, titulo="Menú Administrador", color="\033[1;34m")
 
-        opcion = input("\n Seleccione una opción: ")
+    def run(self):
+        while True:
+            print(self.color)
+            print("\n════════════════════════════════")
+            print("      🛠 MENÚ ADMINISTRADOR      ")
+            print("════════════════════════════════\033[0m")
+            print("1️⃣   Menú de Productos")
+            print("2️⃣   Menú de Clientes")
+            print("3️⃣  🔙 Volver")
+            opcion = input("\nSeleccione una opción: ")
 
-        if opcion == "1":
-            crear_nuevo_producto(con)
-        elif opcion == "2":
-            actualizar_producto(con)
-        elif opcion == "3":
-            consultar_producto(con)
-        elif opcion == "4":
-            break
-        else:
-            print("⚠️ Opción no válida.")
+            if opcion == "1":
+                MenuProductos(self.con).run()
+            elif opcion == "2":
+                MenuClientes(self.con).run()
+            elif opcion == "3":
+                print("👋 Volviendo al menú principal...")
+                sleep(1)
+                break
+            else:
+                print("⚠️ Opción no válida, intente nuevamente.")
 
-# ================= MENÚ DE CLIENTES ==================
-def menu_clientes(con):
-    while True:
-        print("\033[1;36m\n ═════════════════════════════")
-        print("       👥 MENÚ DE CLIENTES       ")
-        print("════════════════════════════ \033[0m")
-        print("1️⃣   Agregar cliente")
-        print("2️⃣   Actualizar dirección")
-        print("3️⃣  🔍 Consultar cliente")
-        print("4️⃣  🔙 Volver al Menú Principal")
+class MenuCliente(Menu):
+    def __init__(self, con):
+        # Configura el menú para clientes 
+        super().__init__(con, titulo="Menú Cliente", color="\033[1;32m")
 
-        opcion = input("\n Seleccione una opción: ")
+    def run(self):
+        # Gestiona registro, actualización y compra
+        while True:
+            print(self.color)
+            print("\n════════════════════════════=")
+            print("        👤 MENÚ CLIENTE        ")
+            print("════════════════════════════=\033[0m")
+            print("1️⃣   Registrarse")
+            print("2️⃣   Actualizar dirección")
+            print("3️⃣   Hacer una compra")
+            print("4️⃣  🔙 Volver")
+            opcion = input("\nSeleccione una opción: ")
 
-        if opcion == "1":
-            crear_nuevo_cliente(con)
-        elif opcion == "2":
-            actualizar_direccion_cliente(con)
-        elif opcion == "3":
-            consultar_cliente(con)
-        elif opcion == "4":
-            break
-        else:
-            print("⚠️ Opción no válida, intente nuevamente.")
+            if opcion == "1":
+                crear_nuevo_cliente(self.con)
+            elif opcion == "2":
+                actualizar_direccion_cliente(self.con)
+            elif opcion == "3":
+                MenuCompras(self.con).run()
+            elif opcion == "4":
+                print("👋 Volviendo al menú principal...")
+                sleep(1)
+                break
+            else:
+                print("⚠️ Opción no válida, intente nuevamente.")
 
-# ================= MENÚ DE COMPRAS ==================
-def menu_compras(con):   
-    carrito = [] 
+class MenuProductos(Menu):
+    def __init__(self, con):
+        #Atributos heredados de Menu()
+        super().__init__(con, titulo="Menú de Productos", color="\033[1;35m")
 
-    while True:
-        if carrito:
-            print("\n🛒 Productos en el carrito:")
-            for i, item in enumerate(carrito):
-                print(f"{i + 1}. {item[1]} unidades de {item[0]} a ${item[2]} cada una.")
-        print("\033[1;31m\n ═════════════════════════════")
-        print("       🛍 MENÚ DE COMPRAS       ")
-        print("════════════════════════════ \033[0m")
-        print("1️⃣  ➕ Añadir producto")
-        print("2️⃣  ➖ Quitar producto")
-        print("3️⃣  ✅ Finalizar compra")
-        print("4️⃣  🔙 Volver")
+    def run(self):
+        while True:
+            print(self.color)
+            print("\n═════════════════════════════════")
+            print("       📦 MENÚ DE PRODUCTOS       ")
+            print("═════════════════════════════════\033[0m")
+            print("1️⃣   Agregar producto")
+            print("2️⃣   Actualizar producto")
+            print("3️⃣  🔍 Consultar producto")
+            print("4️⃣  🔙 Volver al Menú Principal")
+            opcion = input("\nSeleccione una opción: ")
 
-        opcion = input("\n Seleccione una opción: ")
+            if opcion == "1":
+                crear_nuevo_producto(self.con)
+            elif opcion == "2":
+                actualizar_producto(self.con)
+            elif opcion == "3":
+                consultar_producto(self.con)
+            elif opcion == "4":
+                break
+            else:
+                print("⚠️ Opción no válida.")
 
-        if opcion == '1':
-            añadir_producto(carrito, con)
-        elif opcion == '2':
-            quitar_producto(carrito)
-        elif opcion == '3':
-            finalizar_compra(carrito, con)
-        elif opcion == '4':
-            print("🎉 Gracias por su compra 🍻")
-            break
-        else:
-            print("⚠️ Opción no válida.")
+class MenuClientes(Menu):
+    def __init__(self, con):
+        # Configura el menú de clientes (administrador)
+        super().__init__(con, titulo="Menú de Clientes", color="\033[1;36m")
+
+    def run(self):
+        # Permite al administrador gestionar clientes
+        while True:
+            print(self.color)
+            print("\n════════════════════════════")
+            print("       👥 MENÚ DE CLIENTES       ")
+            print("════════════════════════════\033[0m")
+            print("1️⃣   Agregar cliente")
+            print("2️⃣   Actualizar dirección")
+            print("3️⃣  🔍 Consultar cliente")
+            print("4️⃣  🔙 Volver al Menú Principal")
+            opcion = input("\nSeleccione una opción: ")
+
+            if opcion == "1":
+                crear_nuevo_cliente(self.con)
+            elif opcion == "2":
+                actualizar_direccion_cliente(self.con)
+            elif opcion == "3":
+                consultar_cliente(self.con)
+            elif opcion == "4":
+                break
+            else:
+                print("⚠️ Opción no válida, intente nuevamente.")
+
+class MenuCompras(Menu):
+    def __init__(self, con):
+        #Se inicializan atributos heredados y carrito de compras
+        super().__init__(con, titulo="Menú de Compras", color="\033[1;31m")
+        self.carrito = []
+
+    def run(self):
+        while True:
+            if self.carrito:
+                print("\n🛒 Productos en el carrito:")
+                for i, item in enumerate(self.carrito):
+                    print(f"{i + 1}. {item[1]} unidades de {item[0]} a ${item[2]} cada una.")
+            print(self.color)
+            print("\n════════════════════════════=")
+            print("       🛍 MENÚ DE COMPRAS       ")
+            print("════════════════════════════=\033[0m")
+            print("1️⃣  ➕ Añadir producto")
+            print("2️⃣  ➖ Quitar producto")
+            print("3️⃣  ✅ Finalizar compra")
+            print("4️⃣  🔙 Volver")
+            opcion = input("\nSeleccione una opción: ")
+
+            if opcion == '1':
+                añadir_producto(self.carrito, self.con)
+            elif opcion == '2':
+                quitar_producto(self.carrito)
+            elif opcion == '3':
+                finalizar_compra(self.carrito, self.con)
+            elif opcion == '4':
+                print("🎉 Gracias por su compra 🍻")
+                break
+            else:
+                print("⚠️ Opción no válida.")
 
 if __name__ == "__main__":
-    menu_principal()
+    MenuPrincipal().run()
